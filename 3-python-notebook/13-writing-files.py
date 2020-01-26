f83fab658f2baf27cad8d48b43c036e6f0fc6a4f
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2020-01-26 16:21:56 -0500

    Adding one or two things

--- 3-python-notebook/13-writing-files.py
+++ 3-python-notebook/13-writing-files.py
@@ -1,6 +1,28 @@
 file_path = 'file_to_write.txt'
 
-# To write a file using python we'll use the method `open` with the w+ modifier
+# To write a file using python we'll use the method `open`, here's the list of opening modes
+# r   Open text file for reading.  The stream is positioned at the
+#     beginning of the file.
+#
+# r+  Open for reading and writing.  The stream is positioned at the
+#     beginning of the file.
+#
+# w   Truncate file to zero length or create text file for writing.
+#     The stream is positioned at the beginning of the file.
+#
+# w+  Open for reading and writing.  The file is created if it does not
+#     exist, otherwise it is truncated.  The stream is positioned at
+#     the beginning of the file.
+#
+# a   Open for writing.  The file is created if it does not exist.  The
+#     stream is positioned at the end of the file.  Subsequent writes
+#     to the file will always end up at the then current end of file,
+#     irrespective of any intervening fseek(3) or similar.
+#
+# a+  Open for reading and writing.  The file is created if it does not
+#     exist.  The stream is positioned at the end of the file.  Subse-
+#     quent writes to the file will always end up at the then current
+#     end of file, irrespective of any intervening fseek(3) or similar.
 my_file = open(file_path, 'w+')
 
 my_file.write('Line1\n')
--- 3-python-notebook/14-reading-from-url.py
+++ 3-python-notebook/14-reading-from-url.py
@@ -8,7 +8,6 @@ url = 'https://www.theweathernetwork.com/ca/weather/quebec/montreal/'
 # url = 'http://donnees.ville.montreal.qc.ca/dataset/6502942d-788a-43ff-b935-eb7b7d8b58cd/resource/01f91df2-6ed9-47b2-ba85-e6bc9236ac73/download/compteur-eau.csv'
 # url = 'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json'
 # url = 'https://dog.ceo/api/breeds/image/random'
-# url = https://api.exchangerate-api.com/v4/latest/USD
 
 response = urllib.request.urlopen(url)
 data = response.read()
