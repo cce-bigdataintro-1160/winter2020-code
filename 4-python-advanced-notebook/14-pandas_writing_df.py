4be6286d366c23b1ee7b4573b869d7eaa175cfca
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2020-01-28 21:19:05 -0500

    Writing df

--- 4-python-advanced-notebook/14-pandas_writing_df.py
+++ 4-python-advanced-notebook/14-pandas_writing_df.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+# install pyarrow, pandavro
+
 import pandas as pd
+import pandavro as pdx
 
 
 def pretty_print(name, to_print):
@@ -9,9 +12,10 @@ def pretty_print(name, to_print):
 
 
 # Make sure to install xlrd with pip3!!!
-df = pd.read_excel(io='data/titanic/train.xlsx',
-                   sheet_name='Sheet1',
-                   header=0)
+df = pd.read_csv('data/insurance.csv', header=0)
 
-df.to_csv('data/titanic/created_train.csv')
-
+df.to_excel('data/created_train.xlsx')
+df.to_x('data/created_train.xlsx')
+df.to_json('data/created_train.json', orient='records')
+# df.to_parquet('data/created_train.parquet')
+pdx.to_avro('data/created_train.avro', df)
