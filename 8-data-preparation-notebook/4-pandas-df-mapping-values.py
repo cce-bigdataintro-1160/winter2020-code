9b432d95a849b6b2b455b57470ed4b359affc74d
Eduardo Lomonaco <lomonaco.eduardo@gmail.com>
2020-03-10 21:27:13 -0400

    Removed chained assignment

--- 8-data-preparation-notebook/4-pandas-df-mapping-values.py
+++ 8-data-preparation-notebook/4-pandas-df-mapping-values.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-pd.options.mode.chained_assignment = None
-
 spotify = pd.read_csv('data/top2018.csv')
 print(spotify.head(5).to_string())
 
@@ -9,15 +7,15 @@ print(spotify.head(5).to_string())
 spotify['artists'] = spotify['artists'].apply(lambda x: x.upper())
 
 # Adding a new column with the length of the name of each artist
-# spotify['artists_length'] = spotify['artists'].apply(lambda x: len(x))
+spotify['artists_length'] = spotify['artists'].apply(lambda x: len(x))
 
 # Passing a function that identify songs that contain the word 'love' on the name
-# def has_love(x):
-#     if 'love' in x.lower():
-#         return 1
-#     else:
-#         return 0
-#
-# spotify['name_has_love'] = spotify['name'].apply(has_love)
+def has_love(x):
+    if 'love' in x.lower():
+        return 1
+    else:
+        return 0
+
+spotify['name_has_love'] = spotify['name'].apply(has_love)
 
 print(spotify.head(5).to_string())
